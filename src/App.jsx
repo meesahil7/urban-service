b56f522097faf19e50e0242a1ea14ec12fdb0d54
meesahil7 <meesahil7@gmail.com>
2022-12-14 22:57:54 +0530

    day2-task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Login from "./Components/Login/Login";
 import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
+      <Login />
     </div>
   );
 }
