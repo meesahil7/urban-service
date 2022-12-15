2d328c009f70f493f2965f05cbc93bdef5f59849
meesahil7 <meesahil7@gmail.com>
2022-12-15 07:42:50 +0530

    updated package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import MainRoutes from "./Routes/MainRoutes";
-import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      {/* <MainRoutes /> */}
-      <Login />
+      <MainRoutes />
     </div>
   );
 }
