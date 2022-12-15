2b944fe2ca6eb30f8d89faa011574e3e36f6131a
meesahil7 <meesahil7@gmail.com>
2022-12-15 06:37:08 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Login from "./Components/Login/Login";
 import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
-      <Login />
     </div>
   );
 }
