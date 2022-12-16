288e5b57055757dd3a06c5bb3b3cce40b297843d
Vikas Pawar <pawarvikas534@gmail.com>
2022-12-16 22:54:17 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,6 +4,7 @@ import { reducer as AppReducer } from "./App/reducer";
 import { reducer as AuthReducer } from "./Auth/reducer";
 import {reducer as CartReducer}from "./Cart/reducer"
 
+
 const rootReducer = combineReducers({ AppReducer, AuthReducer,CartReducer });
 
 const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
