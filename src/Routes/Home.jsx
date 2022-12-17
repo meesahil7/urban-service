1358762421221644f17c032d6f5409593209bc41
Rajesh Byagalwar <byagalwarrajesh2001@gmail.com>
2022-12-17 14:10:01 +0530

    Ok done

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import LandingPage from "../LandingPage/LandingPage";
-
+import { Box } from "@chakra-ui/react";
+import  { useEffect, useState } from "react";
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  // useEffect(() => {
+  //   if (query === "") {
+  //     setSuggestions([]);
+  //   } else {
+  //     let newListofSuggestions = searchServices
+  //       .filter((item) =>
+  //         item.service.toLowerCase().indexOf(query) !== -1 ? true : false
+  //       )
+  //       .map((item) => item.service);
+  //     setSuggestions(newListofSuggestions);
+  //   }
+  //   setTimeout(() => setLoading(false), 1000);
+  // }, [query]);
   return <div>
     <LandingPage/>
   </div>;
