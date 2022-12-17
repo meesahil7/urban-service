49b576eefbae75723ee70da243ddfc3cd5ccb423
Sahil Khan <107460559+meesahil7@users.noreply.github.com>
2022-12-17 09:46:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-// import { ScrollingExample } from "./Components/PopUpModel";
 import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-      {/* <ScrollingExample /> */}
       <MainRoutes />
-      
     </div>
   );
 }
