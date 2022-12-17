18b983cb4ac8cf827595eb0345932106812d26c9
meesahil7 <meesahil7@gmail.com>
2022-12-17 18:13:36 +0530

    cart

---
+++ src/Components/Cart/CartPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./cartPage.css";
+
+const CartPage = () => {
+  return (
+    <div className="body">
+      <div className="heading">
+        <h1>Summary</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
