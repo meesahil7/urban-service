9376a6aa81f2cd2b71630c34b65a90b51af5c18c
meesahil7 <meesahil7@gmail.com>
2022-12-17 23:14:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Cart from "./Routes/Cart";
 import MainRoutes from "./Routes/MainRoutes";
-import Product from "./Routes/Product";
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <MainRoutes />
     </div>
   );
 }
